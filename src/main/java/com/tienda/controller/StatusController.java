0572c6e4a199a502cffe84032a5af25ed990d47a
Fernando Moncada <josefernandomc@hotmail.es>
2022-11-03 23:14:14 -0500

    Se crea el controlador de status para separar del controlador de productos

---
+++ src/main/java/com/tienda/controller/StatusController.java
@@ -0,0 +1,18 @@
+package com.tienda.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+	@GetMapping("/healthcheck")
+	public String status() {
+		return "OK";
+	}
+	
+	@GetMapping("/error")
+	public String error() {
+		return "Ups hay un error, Cancele";
+	}
+}
